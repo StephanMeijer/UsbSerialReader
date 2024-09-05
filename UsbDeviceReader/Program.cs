391836510b66a26688393d967e1e9f92bfc9a223
Stephan Meijer <me@stephanmeijer.com>
2024-09-06 00:01:55 +0200

    Fixes and clean code

--- UsbDeviceReader/Program.cs
+++ UsbDeviceReader/Program.cs
@@ -8,7 +8,8 @@ using UsbDeviceReader;
 // In this thing, change the path..
 var output = new ShellRunner("cat", "/Users/steve/RiderProjects/UsbDeviceReader/UsbDeviceReader/system_profiler.txt").Run();
 
-string blockPattern = @"(?<=\n\s+)(.+):\n(?:\s{14}.+\n)+";
+// string blockPattern = @"(?<=\n\s+)(.+):\n(?:\s{14}.+\n)+";
+string blockPattern = @"(?<=\n|^)(\s*)(.+):\n(?:\s+.+: .+\n)+";
 Regex blockRegex = new Regex(blockPattern, RegexOptions.Multiline);
 MatchCollection blockMatches = blockRegex.Matches(output);
 
@@ -16,41 +17,28 @@ List<USBDevice> devices = new List<USBDevice>();
 
 foreach (Match blockMatch in blockMatches)
 {
-    var firstLineEnd = blockMatch.Value.IndexOf('\n');
-
-    if (firstLineEnd == -1)
-    {
-        continue;
-    }
-
-    string firstLine = blockMatch.Value.Substring(0, firstLineEnd).Trim();
-
     Dictionary<string, string> propertyMap = new Dictionary<string, string>();
 
-    if (firstLine.EndsWith(":")) // Check if the last character is a colon
-    {
-        firstLine = firstLine.TrimEnd(':'); // Remove the colon from the end
-    }
-
-    propertyMap["Label"] = firstLine;
-
-    string[] otherLines = blockMatch
+    string[] lines = blockMatch
         .Value
-        .Substring(firstLineEnd + 1)
         .Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-    foreach (string line in otherLines)
+    foreach (string line in lines)
     {
         string trimmedLine = line.Trim();
-        if (string.IsNullOrEmpty(trimmedLine))
-        {
-            continue; // Skip empty lines
-        }
 
         int colonIndex = trimmedLine.IndexOf(':');
         if (colonIndex > 0) // Ensure there's a key before the colon
         {
             string key = trimmedLine.Substring(0, colonIndex).Trim();
+            
+            // First line so assume key is the label :)
+            if (!propertyMap.ContainsKey("Label"))
+            {
+                propertyMap["Label"] = key;
+                continue;
+            }
+            
             string value = trimmedLine.Substring(colonIndex + 1).Trim();
             propertyMap[key] = value; // Add to dictionary, assuming unique keys
         }
